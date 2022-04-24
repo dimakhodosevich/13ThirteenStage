bc6ad2ee8ad273c8b4c9d9957998c83640529928
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-24 17:51:53 +0300

    Make test logic for Extreme class.

---
+++ src/by/itstep/khodosevich/thirteenstage/task01/module/logic/Extreme.java
@@ -0,0 +1,15 @@
+package by.itstep.khodosevich.thirteenstage.task01.module.logic;
+
+public class Extreme {
+    public static int findIndexMaxElement(double[] array){
+        return 0;
+    }
+
+    public static int findIndexMinElement(double[] array){
+        return 0;
+    }
+
+    public static int getAverageValue(double[] array){
+        return 0;
+    }
+}

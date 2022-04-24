de86d3fae04a46158b748948ccb17048fe27d3bd
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-24 18:55:54 +0300

    Finish task01. Make controller for it.

--- src/by/itstep/khodosevich/thirteenstage/task01/controller/ControllerExtreme.java
+++ src/by/itstep/khodosevich/thirteenstage/task01/controller/ControllerExtreme.java
@@ -1,5 +1,42 @@
 package by.itstep.khodosevich.thirteenstage.task01.controller;
 
-public class ControllerExtreme {
+import static by.itstep.khodosevich.thirteenstage.task01.module.logic.Extreme.*;
 
+import by.itstep.khodosevich.thirteenstage.view.Printer;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ControllerExtreme {
+    public static final Scanner SCANNER = new Scanner(System.in);
+    public static final Random RANDOM = new Random();
+    public static final int MAX = 25;
+    public static final int MIN = -25;
+
+
+    public static void main(String[] args) {
+        Printer.print("Input size of your array: ");
+        int size = SCANNER.nextInt();
+
+        if (size < 0) {
+            System.err.println("Your array has negative size!!!");
+        }
+
+        double[] array = new double[size];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = RANDOM.nextDouble(MAX - MIN + 1) + MIN;
+        }
+
+        String message = String.format("Your array is: %s.\n", Arrays.toString(array));
+        Printer.print(message);
+        String message1 = String.format("Max element in your array is: %.2f.\n", array[findIndexMaxElement(array)]);
+        Printer.print(message1);
+        String message2 = String.format("Min element in your array is: %.2f.\n", array[findIndexMinElement(array)]);
+        Printer.print(message2);
+        String message3 = String.format("Average value of your array is: %.2f.\n", getAverageValue(array));
+        Printer.print(message3);
+
+    }
 }

04ede3a64ae20c69e987da1932f2e0df73a54188
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-28 23:07:22 +0300

    Finish task04. Create menu, add logic for calculate odd and all numbers in array. Add test for it.

--- src/by/itstep/khodosevich/thirteenstage/task04/module/logic/Parity.java
+++
@@ -1,9 +0,0 @@
-package by.itstep.khodosevich.thirteenstage.task04.module.logic;
-
-public class Parity {
-    private Parity(){}
-
-    public static double calculateSumOfParityNumber(double[] array){
-        return 0;
-    }
-}
--- test/by/itstep/khodosevich/thirteenstage/task04/ParityOddNumberParityTest.java
+++ test/by/itstep/khodosevich/thirteenstage/task04/ParityOddNumberParityTest.java
@@ -1,10 +1,11 @@
 package by.itstep.khodosevich.thirteenstage.task04;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
-import static by.itstep.khodosevich.thirteenstage.task04.module.logic.Parity.*;
 
-public class ParityTest {
+import static org.junit.Assert.*;
+import static by.itstep.khodosevich.thirteenstage.task04.module.logic.ParityOddNumber.*;
+
+public class ParityOddNumberParityTest {
     private static double[] arrayWithAllParityNumber;
     private static double[] arrayWithoutParityNumber;
     private static double expectedAllParity;
@@ -14,36 +15,36 @@ public class ParityTest {
     private static double expectedWithZero;
 
 
-    static{
-        arrayWithAllParityNumber = new double[]{2,4,6,8,12,14};
-        arrayWithoutParityNumber = new double[]{3,5,7,1,11};
+    static {
+        arrayWithAllParityNumber = new double[]{2, 4, 6, 8, 12, 14};
+        arrayWithoutParityNumber = new double[]{3, 5, 7, 1, 11};
         expectedAllParity = 46;
-        expectedWithoutParity = 27;
+        expectedWithoutParity = 0;
         delta = 0.001;
         expectedWithNull = Integer.MIN_VALUE;
-        expectedWithZero = Integer.MIN_VALUE+1;
+        expectedWithZero = Integer.MIN_VALUE + 1;
     }
 
     @Test
-    public void testCalculateSumOfParityNumberPositiveAllParity(){
+    public void testCalculateSumOfParityNumberPositiveAllParity() {
         double actual = calculateSumOfParityNumber(arrayWithAllParityNumber);
         assertEquals(expectedAllParity, actual, delta);
     }
 
     @Test
-    public void testCalculateSumOfParityNumberPositiveWithoutParity(){
+    public void testCalculateSumOfParityNumberPositiveWithoutParity() {
         double actual = calculateSumOfParityNumber(arrayWithoutParityNumber);
         assertEquals(expectedWithoutParity, actual, delta);
     }
 
     @Test
-    public void testCalculateSumOfParityNumberNegativeWithNull(){
+    public void testCalculateSumOfParityNumberNegativeWithNull() {
         double actual = calculateSumOfParityNumber(null);
         assertEquals(expectedWithNull, actual, delta);
     }
 
     @Test
-    public void testCalculateSumOfParityNumberNegativeWithZero(){
+    public void testCalculateSumOfParityNumberNegativeWithZero() {
         double actual = calculateSumOfParityNumber(new double[0]);
         assertEquals(expectedWithZero, actual, delta);
     }

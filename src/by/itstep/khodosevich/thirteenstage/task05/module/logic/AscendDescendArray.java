321020451d0f7f65455fbc919488bcff5d5f7101
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-29 22:19:19 +0300

    Start task05. Make AscendDescendArray class and plugs methods for it. Create unit test for it.

---
+++ src/by/itstep/khodosevich/thirteenstage/task05/module/logic/AscendDescendArray.java
@@ -0,0 +1,12 @@
+package by.itstep.khodosevich.thirteenstage.task05.module.logic;
+
+public class AscendDescendArray {
+
+    public static boolean isAscendArray(double[] array){
+        return true;
+    }
+
+    public static boolean isDescendArray(double[] array){
+        return true;
+    }
+}

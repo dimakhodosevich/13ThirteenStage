f6ab9ebfe2f65c5f69346f4f0436967b208148ee
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-29 23:32:19 +0300

    Start task06, make plug method and unit test for it.

---
+++ src/by/itstep/khodosevich/thirteenstage/task06/module/logic/PalindromeArray.java
@@ -0,0 +1,9 @@
+package by.itstep.khodosevich.thirteenstage.task06.module.logic;
+
+public class PalindromeArray {
+    private PalindromeArray(){}
+
+    public static boolean isPalindromeArray(int[] array){
+        return false;
+    }
+}

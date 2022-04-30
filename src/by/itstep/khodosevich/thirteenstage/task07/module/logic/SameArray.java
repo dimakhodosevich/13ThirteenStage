8adc46c2d581db39aac3979889d56659fe805da1
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-30 11:36:53 +0300

    Start task07. Make SameArray class and plug method for it. Make triple AAA unit test arrange, act, assert.

---
+++ src/by/itstep/khodosevich/thirteenstage/task07/module/logic/SameArray.java
@@ -0,0 +1,9 @@
+package by.itstep.khodosevich.thirteenstage.task07.module.logic;
+
+public class SameArray {
+    private SameArray(){}
+
+    public static boolean isAllElementArrayTheSame(double[] array){
+        return false;
+    }
+}

33efb7d3440dfdeb774bf352bc03ecb788ccfac6
dimakhodosevich <102683640+dimakhodosevich@users.noreply.github.com>
2022-04-30 12:48:34 +0300

    Start task 08. Make unit test(arrange, act, assert). Create plug method to it.

---
+++ src/by/itstep/khodosevich/thirteenstage/task08/module/logic/ParityOddCounter.java
@@ -0,0 +1,17 @@
+package by.itstep.khodosevich.thirteenstage.task08.module.logic;
+
+public class ParityOddCounter {
+    private ParityOddCounter(){}
+
+    public static double getParitySumOfArray(double[] array){
+        return 0;
+    }
+
+    public static double getOddSumOfArray(double[] array) {
+        return 0;
+    }
+
+    public static double getSumOfArray(double[] array){
+        return 0;
+    }
+}
